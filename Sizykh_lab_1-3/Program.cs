509a2e83ab80c4428275bd587a7c069a3f3a6852
hexirk <authirk@yandex.ru>
2022-11-09 21:27:15 +0800

    Лабораторная работа 1.3

---
+++ Sizykh_lab_1-3/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Sizykh_lab_1_3
+{
+    public class Logic
+    {
+        public static string Reverse(string str)
+        {
+            string message = "";
+            string[] words = str.Split(" ");
+            str = "";
+            for (int i = words.Length; i > 0; i--)
+            {
+                str = str + words[i-1] + " ";
+            }
+            message = "Перевернутое предложение: \n" + str;
+            return message;
+        }
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение:");
+            var str = Console.ReadLine();
+            var outMessage = Logic.Reverse(str);
+            Console.WriteLine(outMessage);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sizykh_lab_1-3Tests/LogicTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sizykh_lab_1_3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sizykh_lab_1_3.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void Reverse()
+        {
+            var str = "Проверка предложения";
+            var result = Logic.Reverse(str);
+            Assert.AreEqual("Перевернутое предложение: \nпредложения Проверка ",result);
+        }
+
+        [TestMethod()]
+        public void Reverse1word()
+        {
+            var str = "Проверка";
+            var result = Logic.Reverse(str);
+            Assert.AreEqual("Перевернутое предложение: \nПроверка ", result);
+        }
+    }
+}
